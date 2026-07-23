2aa6be3a452518a45004b0125b1fcdc0bb275c69
Saad Sayyed <saadsyed950@gmail.com>
2026-07-23 21:12:25 +0530

    Update profile picture API

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { generateToken } from "../lib/token.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signUp = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -71,6 +72,39 @@ export const logout = async (req, res) => {
     res.status(204).json({ message: "User logged out" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  const { profilePic } = req.body;
+
+  try {
+    if (!profilePic) {
+      res.status(400).json({ error: "Profile pic is required" });
+    }
+
+    const userId = req.user._id;
+    if (!userId)
+      return res
+        .status(401)
+        .json({ error: "Unauthorized - token not provided" });
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true },
+    );
+
+    res
+      .status(200)
+      .json({
+        message: "User profile picture updated",
+        user: updatedUser.email,
+      });
+  } catch (error) {
+    console.log("Error in updateProfile controller", error.message);
     return res.status(500).json({ error: error.message });
   }
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import * as controllers from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const authRoutes = Router();
 
 authRoutes.post("/signUp", controllers.signUp);
 authRoutes.post("/login", controllers.login);
 authRoutes.post("/logout", controllers.logout);
+
+authRoutes.put("/update-profile", protectRoute, controllers.updateProfile);
 
 export default authRoutes;
