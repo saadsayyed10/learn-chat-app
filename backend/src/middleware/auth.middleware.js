304386f057e6cb7f13925a87e40c896bb07f473b
Saad Sayyed <saadsyed950@gmail.com>
2026-07-23 20:57:25 +0530

    Auth middleware

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model";
+import jwt from "jsonwebtoken";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = res.cookie.jwt;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized - No token provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(401).json({ error: "User not found" });
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Error in auth middleware");
+    return res.status(500).json({ error: error.message });
+  }
+};
