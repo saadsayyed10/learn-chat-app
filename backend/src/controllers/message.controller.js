3db32ca48d8bc9f60e5196f11fb3cefba8f19945
Saad Sayyed <saadsyed950@gmail.com>
2026-07-27 14:33:19 +0530

    Send message API (Before socket.io)

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -31,5 +32,33 @@ export const getMessages = async (req, res) => {
   } catch (error) {
     console.log("Error in getMessages controller", error.message);
     return res.status(400).json({ error: error.message });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image,
+    });
+
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller", error.message);
+    return res.status(500).json({ error: error.message });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,11 +3,14 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getMessages,
   getUsersForSidebar,
+  sendMessage,
 } from "../controllers/message.controller.js";
 
 const messageRoutes = Router();
 
 messageRoutes.get("/users", protectRoute, getUsersForSidebar);
 messageRoutes.get("/:id", protectRoute, getMessages);
+
+messageRoutes.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRoutes;
